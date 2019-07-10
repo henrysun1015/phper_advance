50ab3d25d92835f43846babdcc1a236690e53e6d
shr26207 <sunhaoran@offcn.com>
2019-07-10 13:26:23 +0800

    all

---
+++ Day8_stringTransformer.php
@@ -0,0 +1,29 @@
+<?php
+function stringTransformer($s){
+	$new_string = $word = '';
+	$i = 0;
+	$len = strlen($s);
+	while ($i<$len) {
+		$litter = $s{$i};
+		$i++;
+		$ascii_num = ord($litter);
+		if($ascii_num==32){
+			$word = ' '.$word;
+			if(isset($new_string{0})){
+				$new_string = $word.$new_string;
+			}else{
+				$new_string = $word;
+			}
+			$word ='';
+			continue;
+		}
+		$word .= chr($ascii_num>96?($ascii_num-32):($ascii_num+32));
+
+	}
+	if(!empty($word)){
+		$new_string = $word.$new_string;
+	}
+	return $new_string;
+}
+var_dump(stringTransformer("Example string"));//string(14) "STRING eXAMPLE"
+var_dump(stringTransformer(" A b C d E f G "));//string(15) " g F e D c B a "
